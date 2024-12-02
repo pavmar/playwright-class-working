2438d854f81dcee483e6e39d7738dab5229e416a
Pavan Kumar C S <psiddegowda@clarku.edu>
2024-12-02 15:58:38 -0500

    API test

--- playwright-class/tests/apiResuest.spec.ts
+++ playwright-class/tests/apiResuest.spec.ts
@@ -2,7 +2,37 @@ import { expect } from "@playwright/test";
 import { test } from "../apiRequestFixture";
 
 
-test('test', async ({ apiRequest }) => {
-  const response = await apiRequest.get('/docs/intro')
-  expect(response).toBeOK();
-})
+test('get brands', async ({ apiRequest }) => {
+  const response = await apiRequest.get('/brands')
+  expect(response.ok()).toBeTruthy();
+  expect(await response.json()).toContainEqual(expect.objectContaining({
+    slug: 'forgeflex-tools',
+    name: 'ForgeFlex Tools'
+  }));
+});
+
+
+test('Post brands', async ({ apiRequest }) => {
+    const response = await apiRequest.post('/brands', {
+        data: {
+          name: 'CSPK tools',
+          slug: 'CSPK-tools',
+        }
+      })
+    expect(response.ok()).toBeTruthy();
+});
+
+// test('get brands by id', async ({ apiRequest }) => {
+//     const response = await apiRequest.get('/brands/01je2w03f198ke9bqz189tjbm8');
+//     expect(response.ok()).toBeTruthy();
+//     expect(await response.json()).toContainEqual(expect.any({
+//         id: '01je2w03f198ke9bqz189tjbm8',
+//         name: 'CSPK tools',
+//         slug: 'CSPK-tools'
+//       }));
+// });
+
+
+
+
+
